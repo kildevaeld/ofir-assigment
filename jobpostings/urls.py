a9a1f2367b2ae2f22e82472e869c2abb600f39e0
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-11-28 10:33:05 +0100

    Create an api router and add it to urlpatterns

--- jobpostings/urls.py
+++ jobpostings/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from . import views
+from rest_framework import routers
 
+from . import views, api
+
+router = routers.DefaultRouter()
+router.register(r'jobs', api.JobPostingViewSet)
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name = "jobposting-list"),
+    path("api/", include(router.urls))
 ]
 
 
