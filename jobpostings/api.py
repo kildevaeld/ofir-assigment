d60ef372f7f219195aca7bdca8ccceb530737141
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-11-28 10:32:35 +0100

    Create a viewset and a serializer for the Jobpostings

---
+++ jobpostings/api.py
@@ -0,0 +1,15 @@
+from rest_framework import routers, serializers, viewsets
+from .models import JobPosting
+
+
+class JobPostingSerializer(serializers.HyperlinkedModelSerializer):
+    category = serializers.StringRelatedField(many=False, read_only = True)
+    category_id = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = JobPosting
+        fields = ['title', 'company', "id", "description", "summary", "logo_url", "category", "category_id"]
+        
+        
+class JobPostingViewSet(viewsets.ModelViewSet):
+    queryset = JobPosting.objects.all()
+    serializer_class = JobPostingSerializer
